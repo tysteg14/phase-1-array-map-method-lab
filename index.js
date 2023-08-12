7f9ca3d48732f6d03ab67cbbaf7d3a084cfe7d15
Tyrell <tysteg14@gmail.com>
2023-08-12 16:13:49 -0500

    titlecase

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(function (string) {
+    return string
+      .split(' ')
+      .map(function (word){
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join(' ')
+  });
+};
+
+console.log(titleCased());
